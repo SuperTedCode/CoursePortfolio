366cc429cadb4378643f25cdac009424d7d9211f
SuperTedCode <jamest1903@yahoo.co.uk>
2017-02-15 19:43:34 +0000

    Initial setup of home page and some of webPro1. no assigments/projects or labs added yet.

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+$page_title = 'My Portfolio for BSc in Web Technologies and Programming in GMIT';
+include ('includes/header.html');
+include ('includes/index.html');
+include ('includes/footer.html');
+
+?>
---
+++ webPro1.php
@@ -0,0 +1,8 @@
+<?php
+
+$page_title = 'My Portfolio for BSc in Web Technologies and Programming in GMIT';
+include ('includes/header.html');
+include ('includes/webPro1.html');
+include ('includes/footer.html');
+
+?>
