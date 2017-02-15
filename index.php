52b506cdf8256c6c806f5ff08913399941c2e8de
SuperTedCode <jamest1903@yahoo.co.uk>
2017-02-15 21:09:54 +0000

    Changed flow from index.php to other php pages calling the same header and footer but containing there own html code inside. Also added dropdown pages to nav bar in header.html. Just some text in webPro1.php for now

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
-<?php
+<?php #This is the home page containing the index.html
 
-$page_title = 'My Portfolio for BSc in Web Technologies and Programming in GMIT';
+$page_title = 'My Portfolio for BSc in Web Technologies and Programming in GMIT'; //Page title for header.html reference
 include ('includes/header.html');
 include ('includes/index.html');
 include ('includes/footer.html');
-
 ?>
--- webPro1.php
+++ webPro1.php
@@ -1,8 +1,15 @@
 <?php
-
-$page_title = 'My Portfolio for BSc in Web Technologies and Programming in GMIT';
+$page_title = 'Introduction to Web Programming';
 include ('includes/header.html');
-include ('includes/webPro1.html');
-include ('includes/footer.html');
+?>
 
+<div class="jumbotron">
+  <div class="container text-center">
+    <h1>Introduction to Web Programming</h1>      
+    <p>Below you will find a static shopping website created as part of an assigment, A dynamic recipies website as part of a project and some excercises carried out in the labs </p>
+  </div>
+</div>
+
+<?php
+include ('includes/footer.html');
 ?>
