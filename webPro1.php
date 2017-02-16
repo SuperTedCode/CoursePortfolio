17f5c0d1aa1aef7647b4bd006ac3332dfbd2f775
SuperTedCode <jamest1903@yahoo.co.uk>
2017-02-16 16:39:55 +0000

    Linkedin added to nav-webPro1 added 3 thumbnails shoppper,finefoods,labs with caption desc-shop files added-style updated-header/footer added div wrapper

--- webPro1.php
+++ webPro1.php
@@ -5,9 +5,41 @@ include ('includes/header.html');
 
 <div class="jumbotron">
   <div class="container text-center">
-    <h1>Introduction to Web Programming</h1>      
-    <p>Below you will find a static shopping website created as part of an assigment, A dynamic recipies website as part of a project and some excercises carried out in the labs </p>
+    <h1>Introduction to Web Programming</h1>
+    <h2>Semister one</h2>
+    <p>Below you will find a static shopping website created as part of an assigment, A dynamic recipies website as part of a project and some excercises carried out in the labs during semister one. </p>
   </div>
+</div>
+
+<div class="container-fluid text-center">
+<div class="row">
+    <div class="col-sm-4">
+    <div class="thumbnail">
+      <a href=".\shop\index.html"><h4>Shopping website</h4></a>
+      <div class="caption">
+      	<p>A website had to be created where a user could order goods. The user needed to enter the quantity of items before been able to add the order to a cart using session storage to store the order details. When the order was added to cart the user had to be then taken to a second webpage to display all the orders in a table format showing the item, quantity and price for each. The total price for all order was then displayed at the bottom</p>
+      </div>
+    </div>
+    </div>
+
+    <div class="col-sm-4">
+    <div class="thumbnail">
+      <a href="#"><h4>Fine foods recipe website</h4></a>
+      <div class="caption">
+      	<p>A website had to be created where a user could order goods. The user needed to enter the quantity of items before been able to add the order to a cart using session storage to store the order details. When the order was added to cart the user had to be then taken to a second webpage to display all the orders in a table format showing the item, quantity and price for each. The total price for all order was then displayed at the bottom</p>
+      </div>
+    </div>
+    </div>
+
+    <div class="col-sm-4">
+    <div class="thumbnail">
+      <a href="#"><h4>Lab exercises</h4></a>
+      <div class="caption">
+      	<p>A website had to be created where a user could order goods. The user needed to enter the quantity of items before been able to add the order to a cart using session storage to store the order details. When the order was added to cart the user had to be then taken to a second webpage to display all the orders in a table format showing the item, quantity and price for each. The total price for all order was then displayed at the bottom</p>
+      </div>
+    </div>
+    </div>
+</div>
 </div>
 
 <?php
