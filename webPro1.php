c33ec6539fd5abb35579b194d474f836319c8abc
SuperTedCode <jamest1903@yahoo.co.uk>
2017-02-17 17:44:14 +0000

    Removed shop and added sports shop. Add js folder for bootstrap js and jquery plugin. Footer comment. WebPro1 desc for sports shop. Header html edit script links incl root folder.Style textcenter for a h4

--- webPro1.php
+++ webPro1.php
@@ -7,17 +7,23 @@ include ('includes/header.html');
   <div class="container text-center">
     <h1>Introduction to Web Programming</h1>
     <h2>Semister one</h2>
-    <p>Below you will find a static shopping website created as part of an assigment, A dynamic recipies website as part of a project and some excercises carried out in the labs during semister one. </p>
+    <p>Below you will find a simple sports shopping website created as part of an assigment, A dynamic recipies website as part of a project and some excercises carried out in the labs during semister one. </p>
   </div>
 </div>
 
-<div class="container-fluid text-center">
+<div class="container-fluid">
 <div class="row">
     <div class="col-sm-4">
     <div class="thumbnail">
-      <a href=".\shop\index.html"><h4>Shopping website</h4></a>
+      <a href=".\shop\shop_intro.html"><h4>Sports shop website</h4></a>
       <div class="caption">
-      	<p>A website had to be created where a user could order goods. The user needed to enter the quantity of items before been able to add the order to a cart using session storage to store the order details. When the order was added to cart the user had to be then taken to a second webpage to display all the orders in a table format showing the item, quantity and price for each. The total price for all order was then displayed at the bottom</p>
+      	<p><b>For this assigment the goal was to create a website that had the following;</b></p>
+        <ul>
+          <li>Serveral pages that can be easily accessed through a navigation bar on the left.</li>
+          <li>Contain a table, unordered list and a orderd list.</li>
+          <li>Some styles to animate the table when hovering the mouse over it and various other styles for the links and imagine.</li>
+          <li>Finally a structured theme throughout the webpages having a header, navbar, content and footer sections.</li>
+        </ul>
       </div>
     </div>
     </div>
