8408c434e07ee490dc0616e6885f1b5777941229
SuperTedCode <jamest1903@yahoo.co.uk>
2017-02-25 22:38:07 +0000

    Created php page for cross platform mob app dev called appDev1.php - template only

---
+++ appDev1.php
@@ -0,0 +1,47 @@
+<?php
+$page_title = 'Cross Platform Mobile Application Development';
+include ('includes/header.html');
+?>
+
+<div class="jumbotron">
+  <div class="container text-center">
+    <h1>Cross Platform Mobile Application Development</h1>
+    <h2>Semister one</h2>
+    <p>Below you will find a .......</p>
+  </div>
+</div>
+
+<div class="container-fluid">
+<div class="row">
+    <div class="col-sm-4">
+    <div class="thumbnail">
+      <a href="#"><h4 class="title">Lab exercises</h4></a>
+      <div class="caption">
+      	<p></p>
+      </div>
+    </div>
+    </div>
+
+    <div class="col-sm-4">
+    <div class="thumbnail">
+      <a href="#"><h4 class="title">????</h4></a>
+      <div class="caption">
+        <p></p>
+      </div>
+    </div>
+    </div>
+
+    <div class="col-sm-4">
+    <div class="thumbnail">
+      <a href="#"><h4 class="title">?????</h4></a>
+      <div class="caption">
+      	<p></p>
+      </div>
+    </div>
+    </div>
+</div>
+</div>
+
+<?php
+include ('includes/footer.html');
+?>
